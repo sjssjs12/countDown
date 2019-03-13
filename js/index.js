cfca34a8084b43d8a8b0bb74bf11d3d32dde0c0f
sjssjs12 <287060146@qq.com>
2019-03-13 17:19:21 +0800

    倒计时

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+    /*头部微博显示与隐藏*/
+    $(".offcn_header_wb").hover(function(){
+        $(".top_wb_con").show();
+        $(".top_wb_con").css('zIndex',99);
+    },function(){
+        $(".top_wb_con").hide();
+    })
+    $(".offcn_header_wx").hover(function(){
+        $(".top_wx_con").show();
+        $(".top_wx_con").css('zIndex',99);
+    },function(){
+        $(".top_wx_con").hide();
+    })
+    $(".close-btn").click(function () {
+        $(".layer").hide();
+    });
+})
+
+
